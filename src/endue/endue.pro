f918d5fbdb951e18db9cecea9ae541969c7bf25a
ellie <ellie@pacifist.studio>
2019-09-30 01:09:37 -0700

    Added DrawableNode * clickable with LMB, RMB; colors for each * LMB down on DrawableNode selects it * can drag around while selected

--- src/endue/endue.pro
+++ src/endue/endue.pro
@@ -28,7 +28,8 @@ SOURCES += \
         mainwindow.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+    ui/drawablenode.h
         #glib/glib.h
 
 FORMS += \
--- src/endue/main.cpp
+++ src/endue/main.cpp
@@ -1,15 +1,26 @@
 
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
-#include "mainwindow.h"
+//#include "mainwindow.h"
+#include "ui/drawablenode.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.setWindowTitle(QString("Endue"));
-    w.show();
+    //MainWindow w;
+    //w.setWindowTitle(QString("Endue"));
+    //w.show();
 
+
+    QGraphicsScene scene;
+
+    DrawableNode *circleItem = new DrawableNode(QRect(0, 50, 25, 25));
+    scene.addItem(circleItem);
+
+    QGraphicsView view(&scene);
+    view.show();
 
     return a.exec();
 }
