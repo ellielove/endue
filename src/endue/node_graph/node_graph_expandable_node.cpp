d17f80f2763f9b88a6d1b56f8f6846534d9486e1
ellie <ellie@pacifist.studio>
2019-10-24 16:31:06 -0700

    ExpandableNode Update:
    1. Now can draw itself to screen
    2. Shows demo of expanded vs contracted view

--- src/endue/node_graph/node_graph_expandable_node.cpp
+++ src/endue/node_graph/node_graph_expandable_node.cpp
@@ -1,1 +1,59 @@
 #include "node_graph_expandable_node.h"
+
+NodeGraphExpandableNode::NodeGraphExpandableNode(bool isExpanded, QGraphicsItem *parent) :
+    bIsExpanded(isExpanded),
+    QGraphicsItem(parent)
+{}
+
+void NodeGraphExpandableNode::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    if (bIsExpanded)
+    {
+        painter->setBrush(selected_a);
+        painter->drawEllipse(-100, -50, 200, 100);
+    }
+    else
+    {
+        painter->setBrush(notSelected);
+        painter->drawEllipse(-10, -10, 20, 20);
+    }
+}
+
+QRectF NodeGraphExpandableNode::boundingRect() const
+{
+    if (bIsExpanded)
+    {
+        return QRectF(-100, -50, 200, 100);
+    }
+    else
+    {
+        return QRectF(-10, -10, 20, 20);
+    }
+}
+
+QPainterPath NodeGraphExpandableNode::shape() const
+{}
+
+QPointF NodeGraphExpandableNode::centerPoint() const
+{
+    return QPointF();
+}
+
+void NodeGraphExpandableNode::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{}
+
+void NodeGraphExpandableNode::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{}
+
+void NodeGraphExpandableNode::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{}
+
+void NodeGraphExpandableNode::setExpanded(bool isExpanded)
+{
+    this->bIsExpanded = isExpanded;
+}
+
+bool NodeGraphExpandableNode::isExpanded() const
+{
+    return bIsExpanded;
+}
--- src/endue/node_graph/node_graph_expandable_node.h
+++ src/endue/node_graph/node_graph_expandable_node.h
@@ -1,24 +1,37 @@
 #ifndef NODE_GRAPH_EXPANDABLE_NODE_H
 #define NODE_GRAPH_EXPANDABLE_NODE_H
 
+#include <QPainter>
 #include <QGraphicsItem>
+#include <QGraphicsSceneMouseEvent>
 
 class NodeGraphExpandableNode : public QGraphicsItem
 {
 public:
-    NodeGraphExpandableNode();
+    const QColor notSelected = QColor(102, 159, 250);
+    const QColor selected_a = QColor(186, 240, 125);
+    const QColor selected_b = QColor(245, 197, 66);
+
+    NodeGraphExpandableNode(bool isExpanded, QGraphicsItem *parent = nullptr);
 
     // overrides from QGraphicsItem
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
     QRectF boundingRect() const override;
     QPainterPath shape() const override;
+    QPointF centerPoint() const;
 
     void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
 
+    void setExpanded(bool isExpanded);
+    bool isExpanded() const;
 
-    QColor base_color;
+protected:
+
+
+
+    bool bIsExpanded = false;
     // position in 2d space
     // position in 3d space?
     // isExpanded
@@ -34,6 +47,8 @@ public:
     // node can only display the top 0<=N<=10 connections
 
     // if this node is a definition-head
+
+
 
 };
 
