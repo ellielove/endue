a6227c57c5ca3c06d782adf1aa6238de9c2845ff
ellie <ellie@pacifist.studio>
2019-10-24 16:34:07 -0700

    Added mouse events to NodeGraphGraphicsView
    NOTE: I think we should be using the NGGraphicsView to check for other
    nodes already present in the scene, but I'm not sure.
    
    Ultimately, we'll want the behaviour of: you can create a new node with
    the mouse, but if you click on an existing node, that one is selected
    for editing.

--- src/endue/node_graph/node_graph_graphics_view.cpp
+++ src/endue/node_graph/node_graph_graphics_view.cpp
@@ -1,13 +1,43 @@
 #include "node_graph_graphics_view.h"
 
 
-NodeGraphGraphicsView::NodeGraphGraphicsView(QWidget *parent) :
-    QGraphicsView(parent)
-{
-}
+//NodeGraphGraphicsView::NodeGraphGraphicsView(QWidget *parent) :
+//    QGraphicsView(parent)
+//{}
 
 NodeGraphGraphicsView::NodeGraphGraphicsView(QGraphicsScene *scene, QWidget *parent) :
     QGraphicsView(scene, parent)
 {
     this->setSceneRect(DEFAULT_SCENE_RECT);
 }
+
+void NodeGraphGraphicsView::mousePressEvent(QMouseEvent *event)
+{
+    qDebug() << this->itemAt(event->pos());
+
+    QGraphicsView::mousePressEvent(event);
+}
+
+void NodeGraphGraphicsView::mouseReleaseEvent(QMouseEvent *event)
+{
+    QGraphicsView::mouseReleaseEvent(event);
+}
+
+void NodeGraphGraphicsView::mouseMoveEvent(QMouseEvent *event)
+{
+    QGraphicsView::mouseMoveEvent(event);
+}
+
+///void NodeGraphGraphicsView::createNode(const QPointF position, qreal radius)
+///{
+///    //auto node = new NodeGraphExpandableNode(createExpandedNodes);
+///    //node->setPos(position);
+///    //scene->addItem(node);
+///    ///// contains the right offset for current ellipse
+///    ///const auto ellipseRect = QRectF(
+///    ///      -radius
+///    ///    , -radius * 3.0
+///    ///    ,  radius * 2.0
+///    ///    ,  radius * 2.0
+///    ///);
+///}
--- src/endue/node_graph/node_graph_graphics_view.h
+++ src/endue/node_graph/node_graph_graphics_view.h
@@ -1,7 +1,12 @@
 #ifndef NODE_GRAPH_GRAPHICS_VIEW_H
 #define NODE_GRAPH_GRAPHICS_VIEW_H
 
+#include <QDebug>
+
+#include <QMouseEvent>
 #include <QGraphicsView>
+
+#include <node_graph/node_graph_expandable_node.h>
 
 class NodeGraphGraphicsView : public QGraphicsView
 {
@@ -9,8 +14,13 @@ private:
     const QRectF DEFAULT_SCENE_RECT = QRectF(0, 0, 4000, 4000);
 
 public:
-    explicit NodeGraphGraphicsView(QWidget *parent = nullptr);
     explicit NodeGraphGraphicsView(QGraphicsScene *scene, QWidget *parent = nullptr);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+    //void createNode(const QPointF position, qreal radius);
 };
 
 #endif // NODE_GRAPH_GRAPHICS_VIEW_H
